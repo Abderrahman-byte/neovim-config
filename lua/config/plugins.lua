eeb5413e4119ab0ef0653e6fbcca83da00b2f01c
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-12-31 15:01:39 +0100

    fix : formatted

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,10 +1,11 @@
 local fn = vim.fn
 
 -- Automatically install packer
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '2', 'https://github.com/wbthomason/packer.nvim', install_path})
-  print("Installing packer close and reopen Neovim...")
+	PACKER_BOOTSTRAP =
+		fn.system({ "git", "clone", "--depth", "2", "https://github.com/wbthomason/packer.nvim", install_path })
+	print("Installing packer close and reopen Neovim...")
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -18,98 +19,100 @@ vim.cmd([[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Have packer use a popup window
 packer.init({
-    display = {
-      open_fn = function()
-        return require('packer.util').float({ border = 'single' })
-      end
-    }
-  }
-)
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "single" })
+		end,
+	},
+})
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
-  use 'wbthomason/packer.nvim' -- Have packer manage itself
-  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
+	-- My plugins here
+	use("wbthomason/packer.nvim") -- Have packer manage itself
+	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
+	use("nvim-lua/plenary.nvim") -- Useful lua functions used by lots of plugins
 
-  use "kyazdani42/nvim-web-devicons"
+	use("kyazdani42/nvim-web-devicons")
 
-  use {
-      "nvim-lualine/lualine.nvim",
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-  }
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
 
-  -- Tree File Explorer
-  use {
-      "kyazdani42/nvim-tree.lua",
-      requires = { "kyazdani42/nvim-web-devicons" },
-  }
+	-- Tree File Explorer
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = { "kyazdani42/nvim-web-devicons" },
+	})
 
-  -- Themes
-  use 'folke/tokyonight.nvim'	
-  use 'EdenEast/nightfox.nvim'
-  use 'Mofiqul/dracula.nvim'
-  use 'projekt0n/github-nvim-theme'
-  use 'rakr/vim-one'
-  use 'ayu-theme/ayu-vim'
+	-- Themes
+	use("folke/tokyonight.nvim")
+	use("EdenEast/nightfox.nvim")
+	use("Mofiqul/dracula.nvim")
+	use("projekt0n/github-nvim-theme")
+	use("rakr/vim-one")
+	use("ayu-theme/ayu-vim")
 
-  -- Treesitter
-  use {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-  }
-    -- Telescope
-  use {
-    'nvim-telescope/telescope.nvim', 
-    tag = '0.1.0',
-    requires = { 'nvim-lua/plenary.nvim' },
-  }
-  use { "nvim-telescope/telescope-file-browser.nvim" }
+	-- Treesitter
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
+	-- Telescope
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		requires = { "nvim-lua/plenary.nvim" },
+	})
+	use({ "nvim-telescope/telescope-file-browser.nvim" })
 
-  -- Latex
-  use 'lervag/vimtex'
+	-- Latex
+	use("lervag/vimtex")
 
-  -- Show git change (change, delete, add) signs in vim sign column
-  use "lewis6991/gitsigns.nvim"
+	-- Show git change (change, delete, add) signs in vim sign column
+	use("lewis6991/gitsigns.nvim")
 
-  -- Automatic insertion and deletion of a pair of characters
-  use { "Raimondi/delimitMate", event = "InsertEnter" }
+	-- Automatic insertion and deletion of a pair of characters
+	use({ "Raimondi/delimitMate", event = "InsertEnter" })
 
-  -- Python indent (follows the PEP8 style)
-  use { "Vimjas/vim-python-pep8-indent", ft = { "python" } }
+	-- Python indent (follows the PEP8 style)
+	use({ "Vimjas/vim-python-pep8-indent", ft = { "python" } })
 
-  -- LSP plugins
-  use {
-  'VonHeikemen/lsp-zero.nvim',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
+	-- Auto format
+	use({ "sbdchd/neoformat", cmd = { "Neoformat" } })
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-buffer'},
-    {'hrsh7th/cmp-path'},
-    {'saadparwaiz1/cmp_luasnip'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/cmp-nvim-lua'},
+	-- LSP plugins
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-    -- Snippets
-    {'L3MON4D3/LuaSnip'},
-    {'rafamadriz/friendly-snippets'},
-    }
-  }
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require('packer').sync()
-  end
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	})
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
