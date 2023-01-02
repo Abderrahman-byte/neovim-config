e8154aa235c126f13b1ecc895f4bae67ff66a46e
Abderrahman-byte <el.abderrahman00@gmail.com>
2023-01-02 21:13:00 +0100

    add vimtex keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<C-i>", ":Neoformat<CR>", {})
 vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("n", "<leader>ll", ":VimtexCompile<Cr>", {})
+vim.keymap.set("n", "<leader>le", ":VimtexErrors<Cr>", {})
