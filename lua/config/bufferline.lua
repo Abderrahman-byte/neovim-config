89fdfb612b18fa7b65be02d17d8fdf433afde04d
Abderrahman-byte <el.abderrahman00@gmail.com>
2023-01-07 10:07:24 +0100

    bufferline settings

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -1,5 +1,18 @@
 require("bufferline").setup({
 	options = {
 		numbers = "buffer id",
+		buffer_close_icon = "",
+		show_buffer_close_icons = false,
+		show_close_icon = false,
+		close_icon = "",
+		separator_style = "slant",
+		offsets = {
+			{
+				filetype = "NvimTree",
+				text = "File Explorer",
+				highlight = "Directory",
+				separator = true, -- use a "true" to enable the default, or set your own character
+			},
+		},
 	},
 })
