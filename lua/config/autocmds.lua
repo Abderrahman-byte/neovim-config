0111faee24026d63c678e8b0d6b5d31055c4d163
Abderrahman-byte <el.abderrahman00@gmail.com>
2023-01-07 03:07:05 +0100

    feat: added some autocommands

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,35 @@
+-- show cursor line only in active window
+local cursorGrp = vim.api.nvim_create_augroup("CursorLine", {
+	clear = true,
+})
+
+vim.api.nvim_create_autocmd(
+	{ "InsertLeave", "WinEnter" },
+	{ pattern = "*", command = "set cursorline", group = cursorGrp }
+)
+
+vim.api.nvim_create_autocmd(
+	{ "InsertEnter", "WinLeave" },
+	{ pattern = "*", command = "set nocursorline", group = cursorGrp }
+)
+
+-- Highlight on yank
+
+local yankGrp = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	command = "silent! lua vim.highlight.on_yank()",
+	group = yankGrp,
+})
+
+--  Auto formatting before save
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	command = ":Neoformat",
+})
+
+-- Center cursor on Insert Enter
+vim.api.nvim_create_autocmd("InsertEnter", {
+	pattern = "*",
+	command = ":normal zz",
+})
