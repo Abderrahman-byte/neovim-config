3e622b1e055c788f5e6a20d77e73fcb36cc9dc69
Abderrahman-byte <el.abderrahman00@gmail.com>
2023-01-19 20:15:09 +0100

    feat: disable arrow keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,15 @@ vim.keymap.set("n", "<leader>le", ":VimtexErrors<Cr>", {})
 vim.keymap.set("n", "<leader>j", ":bprevious<CR>")
 vim.keymap.set("n", "<leader>k", ":bnext<CR>")
 vim.keymap.set("n", "<leader>bc", ":bdelete<CR>")
+
+-- Remove newbie crutches in Normal Mode
+vim.keymap.set("n", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Right>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Up>", "<Nop>", { noremap = true, silent = true })
+
+-- Remove newbie crutches in Visual Mode
+vim.keymap.set("v", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("v", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("v", "<Right>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("v", "<Up>", "<Nop>", { noremap = true, silent = true })
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -277,8 +277,8 @@ vim.cmd [[augroup packer_load_aucmds]]
 vim.cmd [[au!]]
   -- Filetype lazy-loads
 time([[Defining lazy-load filetype autocommands]], true)
-vim.cmd [[au FileType markdown ++once lua require("packer.load")({'markdown-preview.nvim'}, { ft = "markdown" }, _G.packer_plugins)]]
 vim.cmd [[au FileType python ++once lua require("packer.load")({'vim-python-pep8-indent'}, { ft = "python" }, _G.packer_plugins)]]
+vim.cmd [[au FileType markdown ++once lua require("packer.load")({'markdown-preview.nvim'}, { ft = "markdown" }, _G.packer_plugins)]]
 time([[Defining lazy-load filetype autocommands]], false)
   -- Event lazy-loads
 time([[Defining lazy-load event autocommands]], true)
